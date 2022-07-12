11f706cc3945b68ec0f185abc9b555ac3f4ce778
jesus <a20196324@pucp.edu.pe>
2022-07-12 14:28:53 -0500

    admin parte1

--- src/main/java/com/example/ef_g4/servlets/AdminServlet.java
+++ src/main/java/com/example/ef_g4/servlets/AdminServlet.java
@@ -1,6 +1,7 @@
 package com.example.ef_g4.servlets;
 
 import com.example.ef_g4.Beans.Empleado;
+import com.example.ef_g4.Daos.AdminDao;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,14 +15,15 @@ public class AdminServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         Empleado usuario = (Empleado) request.getSession().getAttribute("usuarioLogueado");
-
+        AdminDao adminDao = new AdminDao();
         String action = request.getParameter("admin") == null? "registros" : request.getParameter("admin");
         //System.out.println(usuarioLogueado.getRol());
         //if (Objects.equals(usuarioLogueado.getRol(), "admin")){
             switch(action) {
                 case "registros":
-                    //ArrayList<BSedeYSala> listaSedes = AdminDao.obtenerSedes();
-                    //request.setAttribute("listaSedes",listaSedes);
+
+                    request.setAttribute("valor1",adminDao.valor1());
+                    request.setAttribute("valor2",adminDao.valor2());
                     RequestDispatcher rd1 = request.getRequestDispatcher("/admin.jsp");
                     rd1.forward(request, response);
             }
